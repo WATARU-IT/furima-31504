117e3180549840b17f56b8d9d8b4aeb825730d85
WATARU-IT <disney.kw@gmail.com>
2021-03-20 20:41:25 +0900

    validates書き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-has_many :items
 
   with_options presence: true do
     validates :nickname
-    validates :family_name, format: { with: /\A[一-龥]+\z/, message: 'は全角文字を使用してください' }
-    validates :first_name, format: { with: /\A[一-龥]+\z/, message: 'は全角文字を使用してください' }
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナ文字を使用してください' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナ文字を使用してください' }
+    with_options format:{ with: /\A[一-龥]+\z/, message: 'は全角文字を使用してください' } do
+      validates :family_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナ文字を使用してください' } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
     validates :encrypted_password, :password, length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: 'Include both letters and numbers' }
     validates :birth_day
   end
