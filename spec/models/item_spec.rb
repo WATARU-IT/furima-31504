3436edd18e5ae4333add1e2b6a74895b5ce3b432
WATARU-IT <disney.kw@gmail.com>
2021-03-27 15:16:46 +0900

    商品出品機能修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Item, type: :model do
   describe '商品出品機能' do
     before do
       @item = FactoryBot.build(:item)
-      # binding.pry
     end
 
     it '全ての項目が存在すれば登録できること' do
@@ -16,6 +15,12 @@ RSpec.describe Item, type: :model do
       @item.name = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'imageが空では登録できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
     end
 
     it 'textが空では登録できないこと' do
